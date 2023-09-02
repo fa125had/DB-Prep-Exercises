5ca1e130913d230bd441c1d11743e18ac6cf184d
farshad <far5had@outlook.com>
2023-09-02 13:07:19 +0200

    added queriesv1, week3 questions

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # ERD Diagram
 
 <img src="./assets/HackYourKitchen-ERD.png" alt="ERD Diagram" style="max-width:500px;">
+
+### Normal Forms
+
+#### Was the database already in 2NF / 3 NF?
+
+- The database is in 2NF for the following reasons:
+
+  - **It is in 1NF because:**
+      1. **Single valued columns**: Each column contains atomic, indivisible values.
+      2. **Unique names for columns**: Each column has a unique name to identify its data.
+      3. **Order (of rows/columns) does not matter**: The order in which data is stored does not affect the integrity of the database.
+      4. **Primary Key**: Each table has a primary key that uniquely identifies each record, ensuring that there are no duplicate records.
+      5. **No duplicate records (every record has a primary key)**: This ensures data integrity and allows for efficient data retrieval.
+      6. **Column domain (for any column) should not change**: The data type of a column remains consistent, ensuring data integrity.
+
+  - **It is in 2NF because:**  
+      1. **Fully Functionally Dependent**: All non-key attributes are fully functionally dependent on the primary key. This means there is no partial dependency of any column on the primary key.
--- queries.js
+++ queries.js
@@ -20,7 +20,7 @@ connection.connect((err) => {
   const sqlFile = "hack-your-kitchen.sql";
   const dbName = "hackYourKitchen";
 
-  // read the world.sql
+  // read the SQL file
   fs.readFile(sqlFile, "utf8", (err, data) => {
     if (err) {
       console.error(`Error reading the ${sqlFile}:`, err.message);
@@ -49,6 +49,53 @@ connection.connect((err) => {
 
           console.log(result);
         });
+
+        // All the vegetarian recipes with potatoes
+        connection.query(
+          `
+            SELECT Recipes.RecipeName
+            FROM Recipes
+            JOIN RecipeCategories ON Recipes.RecipeID = RecipeCategories.RecipeID
+            JOIN Categories ON RecipeCategories.CategoryID = Categories.CategoryID
+            JOIN RecipeIngredients ON Recipes.RecipeID = RecipeIngredients.RecipeID
+            JOIN Ingredients ON RecipeIngredients.IngredientID = Ingredients.IngredientID
+            WHERE Categories.CategoryName = 'Vegetarian' AND Ingredients.IngredientName = 'Potatoes';
+          `,
+          (err, result) => {
+            if (err) throw err;
+            console.log("Vegetarian recipes with potatoes:", result);
+          }
+        );
+
+        // All the cakes that do not need baking
+        connection.query(
+          `
+            SELECT Recipes.RecipeName
+            FROM Recipes
+            JOIN RecipeCategories ON Recipes.RecipeID = RecipeCategories.RecipeID
+            JOIN Categories ON RecipeCategories.CategoryID = Categories.CategoryID
+            WHERE Categories.CategoryName = 'Cake' AND Categories.CategoryName != 'No-Bake';
+          `,
+          (err, result) => {
+            if (err) throw err;
+            console.log("Cakes that do not need baking:", result);
+          }
+        );
+
+        // All the vegan and Japanese recipes
+        connection.query(
+          `
+            SELECT Recipes.RecipeName
+            FROM Recipes
+            JOIN RecipeCategories ON Recipes.RecipeID = RecipeCategories.RecipeID
+            JOIN Categories ON RecipeCategories.CategoryID = Categories.CategoryID
+            WHERE Categories.CategoryName = 'Vegan' OR Categories.CategoryName = 'Japanese';
+          `,
+          (err, result) => {
+            if (err) throw err;
+            console.log("Vegan and Japanese recipes:", result);
+          }
+        );
 
         //Disconnecting from the server
         connection.end((err) => {
